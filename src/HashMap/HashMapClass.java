1ee2a127eeae8cd0b4f9ca03eb0543352f7f7c89
timofey <gusev.t.s1@gmail.com>
2018-04-15 11:45:17 +0300

    v 0.3

--- src/HashMap/HashMapClass.java
+++ src/HashMap/HashMapClass.java
@@ -53,10 +53,12 @@ public class HashMapClass<E> {
     private int threshold;
     private int size;
     private int capacity = 16;
+    private int loadListNumber;
 
     HashMapClass() {
         threshold = (int) (capacity * loadFactor);
         hashMap = new Object[capacity];
+        loadListNumber = getLoadListNumber();
     }
 
     HashMapClass(int capacity) {
@@ -64,6 +66,7 @@ public class HashMapClass<E> {
             this.capacity = capacity;
         threshold = (int) (capacity * loadFactor);
         hashMap = new Object[capacity];
+        loadListNumber = getLoadListNumber();
     }
 
     HashMapClass(int capacity, double loadFactor) {
@@ -73,6 +76,51 @@ public class HashMapClass<E> {
             this.loadFactor = loadFactor;
         threshold = (int) (capacity * loadFactor);
         hashMap = new Object[capacity];
+        loadListNumber = getLoadListNumber();
+    }
+
+    public E getValue(E key) {
+        E value = null;
+        E e = null;
+        if (key == null) {
+            e = doSearchNullKey();
+        } else {
+            int hash = doHash(key, value);
+            int index = searchPosition(hash, hashMap.length);
+
+            if (hashMap[index] == null) {
+                System.out.println("no such element");
+                return null;
+            }
+            Node<E> node = (Node<E>) hashMap[index];
+            while (node.next != null) {
+                if (node.key == key)
+                    e = node.value;
+                node = node.next;
+            }
+
+            if(node.key == key)
+                e = node.value;
+        }
+        return e;
+    }
+
+    private E doSearchNullKey() {
+        Node<E> node = (Node<E>) hashMap[0];
+
+        if (hashMap[0] == null) {
+            System.out.println("no such element");
+            return null;
+        }
+        while (node.next != null) {
+            if (node.key == null)
+                return node.value;
+            node = node.next;
+        }
+        if (node.key == null)
+            return node.value;
+
+        return null;
     }
 
     public void addElement(E key, E value) {
@@ -81,7 +129,6 @@ public class HashMapClass<E> {
         else {
             int hash = doHash(key, value);
             int index = searchPosition(hash, hashMap.length);
-
             if (hashMap[index] == null) {
                 addNode(hash, key, value, index);
             } else {
@@ -98,15 +145,12 @@ public class HashMapClass<E> {
                     otherNode.value = value;
                     isSet = true;
                 }
-
                 if (!isSet) {
                     addNode(hash, key, value, index);
                 }
             }
-            size++;
         }
-
-
+        size++;
     }
 
     private void putForNullKey(E value) {
@@ -138,7 +182,7 @@ public class HashMapClass<E> {
     }
 
     private int doHash(E key, E value) {
-        int h = key.hashCode() ^ value.hashCode();
+        int h = key.hashCode();
         h ^= (h >>> 20) ^ (h >>> 12);
         return h ^ (h >>> 7) ^ (h >>> 4);
     }
@@ -168,9 +212,44 @@ public class HashMapClass<E> {
     public void deleteElement(E key, E value) {
         if (key == null)
             removeNullKeyElement(key, value);
-        else{
+        else {
+            int hash = doHash(key, value);
+            int index = searchPosition(hash, hashMap.length);
 
+            if (hashMap[index] == null) {
+                System.out.println("no such element");
+                return;
+            }
+            boolean isDel = false;
+            Node<E> node = (Node<E>) hashMap[index];
+
+            if (node.next == null && (node.key == key && node.value == value)) {
+                hashMap[index] = null;
+                size--;
+                return;
+            }
+            while (node.next != null) {
+                if (node.key == key && node.value == value) {
+                    if (node.next.next == null) {
+                        hashMap[index] = node.next;
+                    } else {
+                        node.next = node.next.next;
+                    }
+                    isDel = true;
+                }
+                node = node.next;
+            }
+            if (node.key == key && node.value == value) {
+                Node<E> otherNode = (Node<E>) hashMap[index];
+                while (otherNode.next.next != null)
+                    otherNode = otherNode.next;
+                otherNode.next = null;
+                isDel = true;
+            }
+            if (!isDel)
+                System.out.println("no such element");
         }
+        size--;
     }
 
     private void removeNullKeyElement(E key, E value) {
@@ -180,20 +259,39 @@ public class HashMapClass<E> {
         }
         Node<E> node = (Node<E>) hashMap[0];
 
+        if (node.next == null && (node.key == key && node.value == value)) {
+            hashMap[0] = null;
+            return;
+        }
+
         while (node.next != null) {
             if (node.key == null) {
-                node.next = node.next.next;
-                return;
+                if (node.next.next == null)
+                    hashMap[0] = node.next;
+                else {
+                    node.next = node.next.next;
+                }
             }
             node = node.next;
         }
-        if(node.key == null){
-            Node<E> node1 = (Node<E>) hashMap[0];
-            while (node1.next != null){
-                if(node1.next.next == null)
-                    node1.next = null;
-            }
+        if (node.key == null) {
+            Node<E> otherNode = (Node<E>) hashMap[0];
+            while (otherNode.next.next != null)
+                otherNode = otherNode.next;
+            otherNode.next = null;
         }
+    }
+
+    public int getNumberOfElements() {
+        return size;
+    }
+
+    public double getLoadFactor() {
+        return loadFactor;
+    }
+
+    public void setLoadFactor(double loadFactor) {
+        this.loadFactor = loadFactor;
     }
 
     public void printHashMap() {
@@ -211,5 +309,28 @@ public class HashMapClass<E> {
             System.out.println((i + 1) + " " + (count + 1) + " - " + node.key + " " + node.value);
         }
         System.out.println();
+    }
+
+    public double getLoadList() {
+
+        double loadList = size / getLoadListNumber();
+        return loadList;
+    }
+
+    public int getLoadListNumber() {
+        int k = 0;
+        for (int i = 0; i < hashMap.length; i++) {
+            if (hashMap[i] != null)
+                k++;
+        }
+        return k;
+    }
+
+    public void removeAllElements() {
+        for (int i = 0; i < hashMap.length; i++) {
+            if (hashMap[i] != null)
+                hashMap[i] = null;
+        }
+        size = 0;
     }
 }
--- src/HashMap/Main.java
+++ src/HashMap/Main.java
@@ -5,8 +5,25 @@ public class Main {
         HashMapClass hashMap = new HashMapClass();
         hashMap.addElement("0", "zero");
         hashMap.addElement("key", "one");
-        hashMap.addElement(null,null);
+        hashMap.addElement(null, null);
         hashMap.addElement("idx", "two");
+        System.out.println(hashMap.getValue(null));
+        System.out.println(hashMap.getValue("0"));
+        System.out.println(hashMap.getLoadList());
+        System.out.println(hashMap.getNumberOfElements());
+        hashMap.printHashMap();
+        hashMap.deleteElement("key", "one");
+        System.out.println(hashMap.getNumberOfElements());
+        hashMap.printHashMap();
+        hashMap.deleteElement(null, null);
+        System.out.println(hashMap.getNumberOfElements());
+        hashMap.printHashMap();
+        hashMap.deleteElement("idx", "two");
+        hashMap.printHashMap();
+        System.out.println(hashMap.getLoadFactor());
+        hashMap.setLoadFactor(2.0);
+        System.out.println(hashMap.getLoadList());
+        hashMap.removeAllElements();
         hashMap.printHashMap();
     }
 }
